659b54ee51a9c6b05caf75a778cbe1e4c2b66373
sekoudosso82 <sekou.dosso82@gmail.com>
2020-10-01 08:00:00 -0400

    Queue in java

---
+++ dataStructure/src/QueueInJava.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueInJava {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList();
+
+        // Enqueue items
+        queue.add("Sam");
+        queue.add("Anna");
+        queue.add("Heidi");
+        queue.add("Susan");
+        queue.add("Charlotte");
+
+        System.out.println(queue);
+
+        String nextInQueue = queue.peek();
+        System.out.println("Next up! "+ nextInQueue);
+        String recentlyRemoved = queue.remove();
+        System.out.println(recentlyRemoved);
+        System.out.println(queue);
+
+    }
+}
