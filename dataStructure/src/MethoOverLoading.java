29ceeee5a26dbea4e6ee6bbd535811fda7dfde6e
sekoudosso82 <sekou.dosso82@gmail.com>
2020-10-19 09:51:05 -0400

    abstraction

---
+++ dataStructure/src/MethoOverLoading.java
@@ -0,0 +1,32 @@
+public class MethoOverLoading {
+
+//        void sum(int a,long b){System.out.println(a+b);}
+//        void sum(int a,int b,int c){System.out.println(a+b+c);}
+//
+//        void summ(int a,long b){System.out.println("a method invoked");}
+//        void summ(long a,int b){System.out.println("b method invoked");}
+
+        public static void main(String args[]){
+//            MethoOverLoading obj=new MethoOverLoading();
+//            obj.sum(20,20);//now second int literal will be promoted to long
+//            obj.sum(20,'c');//now second char literal will be promoted to long
+////            obj.sum(20,20.09);//won't compile bcs second double can not be promoted to long
+//
+//            obj.sum(20,20,20);
+//
+//            // ambiguity long can not be promoted in int
+//
+//
+//            MethoOverLoading obj1=new MethoOverLoading();
+//              obj1.summ(20,20);//now ambiguity
+//                    There are two methods defined with the same name, i.e., summ.
+//                    The first method accepts the integer and long type whereas
+//                    the second method accepts long and the integer type. The parameter
+//                    passed that are a = 20, b = 20. We can not tell that which method
+//                    will be called as there is no clear differentiation mentioned between
+//                    integer literal and long literal. This is the case of ambiguity. Therefore,
+//                    the compiler will throw an error.
+
+        }
+
+}
